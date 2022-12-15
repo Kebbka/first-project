21b532d405b77d916f8e481c7b246f6f49a2d451
kps <x-bar@ya.ru>
2022-12-15 21:16:24 +0300

    обновление файлов

--- Praktikum.java
+++ Praktikum.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class Praktikum {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Converter convert = new Converter(78.5,88.7,0.75);
+
+
+
+
     }
 }
